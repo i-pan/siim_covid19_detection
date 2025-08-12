a6b546919474dcfd8ff3bc0c81a5d0a291f923a8
Ian Pan <i-pan@users.noreply.github.com>
2025-08-12 18:50:06 -0400

    update args

--- ian-siim/detect/main.py
+++ ian-siim/detect/main.py
@@ -27,8 +27,8 @@ def parse_args():
     parser.add_argument('--save-preds-file', type=str)
     parser.add_argument('--rank-average', action='store_true')
     parser.add_argument('--local_rank', type=int, default=0)
-    parser.add_argument('--gpus', type=int, default=1)
-    parser.add_argument('--precision', type=int, default=16)
+    parser.add_argument('--devices', type=int, default=1)
+    parser.add_argument('--precision', type=str, default="16-mixed")
     parser.add_argument('--num_workers', type=int, default=2)
     return parser.parse_args()
 
